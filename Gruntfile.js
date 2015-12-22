889f0b7a841e4f9db1ceb03b97eaf23fece1d50a
Khodor <khodor@prototype.ae>
2015-12-22 13:41:25 +0400

    sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,76 +7,79 @@ module.exports = function(grunt) {
         concat: {
             dist: {
                 src: [
+                    'assets/lib/jquery/dist/jquery.min.js',
                     'assets/js/main.js'
                 ],
-                dest: 'build/js/main.js',
+                dest: 'build/js/scripts.js',
             }
         },
 
 
         uglify: {
             build: {
-                src: 'build/js/main.js',
-                dest: 'build/js/main.js'
+                src: 'build/js/scripts.js',
+                dest: 'build/js/scripts.min.js'
             },
         },
 
 
-        less: {
-            development: {
+
+
+        sass: {
+            dist: {
                 options: {
-                    compress: false,
-                    yuicompress: false,
-                    optimization: 2,
-                    relativeUrls: true,
-                    cleancss: false
+                    loadPath: './',
+                    compass: true,
+                    sourcemap: 'none',
+                    style: 'compressed'
                 },
                 files: {
-                    "build/css/main.css": "assets/css/main.less"
-                }
-            },
-            production: {
-                options: {
-                    compress: true,
-                    yuicompress: true,
-                    optimization: 2,
-                    relativeUrls: true,
-                    cleancss: true
-                },
-                files: {
-                    "build/css/main.css": "assets/css/main.less"
+                    'build/css/style.css': 'assets/sass/style.scss'
                 }
             }
         },
 
+
         watch: {
             styles: {
-                files: ['assets/css/*.less'],
-                tasks: grunt.option('env') == "development" ? ['less:development'] : ['less:production'],
+                files: ['assets/sass/*.scss'],
+                tasks: ['sass'],
                 options: {
                     nospawn: true
                 }
             },
             scripts: {
                 files: ['assets/js/*.js'],
-                tasks: grunt.option('env') == "development" ? ['concat'] : ['concat', 'uglify'],
+                tasks: ['concat', 'uglify'],
                 options: {
                     spawn: false,
                 }
             }
-        }
+        },
+
+        browserSync: {
+            bsFiles: {
+                src: 'build/css/*.css'
+            },
+            options: {
+                server: {
+                    baseDir: "./"
+                },
+                watchTask: true,
+                //proxy: "localhost:90"
+            }
+        },
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    if (grunt.option('env') == "development") {
-        grunt.registerTask('default', ['concat:dist', 'less:development', 'watch']);
-    } else {
-        grunt.registerTask('default', ['concat:dist', 'uglify:build', 'less:production', 'watch']);
-    }
+
+    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'browserSync', 'watch']);
+
 };
---
+++ config.rb
@@ -0,0 +1,4 @@
+preferred_syntax = :scss
+http_path = '/'
+images_dir = 'build/images'
+output_style = :compressed
