4b5aab55095540f518f43026edf3e10e2d5a23bf
Khodor <khodor@prototype.ae>
2017-07-05 09:38:11 +0400

    updgraded to webpack 3

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,6 +18,7 @@ module.exports = {
         }]
     },
     plugins: [
+        new webpack.optimize.ModuleConcatenationPlugin(),
         new webpack.LoaderOptionsPlugin({
             minimize: true,
             debug: false
